4f4f6c56a727ca580701d24cb5f595013f45779f
sonwonjae <sonwj091552@gmail.com>
2024-09-19 22:20:29 +0900

    feat: location 로직 정리

--- apps/server-core/src/locations/locations.controller.ts
+++ apps/server-core/src/locations/locations.controller.ts
@@ -1,17 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { LocationsService } from './locations.service';
-import { CreateLocationDto } from './dto/create-location.dto';
-import { User } from 'src/users/users.decorator';
-import { Tables } from 'src/supabase/supabase.types';
 import {
   GetLocationParamDto,
   GetLocationQueryDto,
@@ -21,36 +9,11 @@ import {
 export class LocationsController {
   constructor(private readonly locationsService: LocationsService) {}
 
-  @Post()
-  create(@Body() body: CreateLocationDto, @User() user: Tables<'users'>) {
-    return this.locationsService.create(body, user);
-  }
-
-  @Get()
-  findAll() {
-    return this.locationsService.findAll();
-  }
-
   @Get('list/:x/:z')
   findLocationListPagination(
     @Param() param: GetLocationParamDto,
     @Query() query: GetLocationQueryDto,
   ) {
     return this.locationsService.findLocationListPagination(param, query);
-  }
-
-  @Get(':x/:z')
-  findOne(@Param() param: GetLocationParamDto) {
-    return this.locationsService.findOne(param);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.locationsService.update(+id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.locationsService.remove(+id);
   }
 }
--- apps/server-core/src/locations/locations.service.ts
+++ apps/server-core/src/locations/locations.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateLocationDto } from './dto/create-location.dto';
 import { SupabaseService } from 'src/supabase/supabase.service';
-import { Tables } from 'src/supabase/supabase.types';
 import {
   GetLocationParamDto,
   GetLocationQueryDto,
@@ -10,19 +8,6 @@ import {
 @Injectable()
 export class LocationsService {
   constructor(private readonly supabaseService: SupabaseService) {}
-
-  async create(body: CreateLocationDto, user: Tables<'users'>) {
-    const { x, z } = body;
-
-    const supabase = this.supabaseService.getClient();
-    console.log(user, supabase, x, z);
-
-    return true;
-  }
-
-  findAll() {
-    return `This action returns all locations`;
-  }
 
   async findLocationListPagination(
     { x, z }: GetLocationParamDto,
@@ -41,20 +26,5 @@ export class LocationsService {
     return (locations || []).map(({ landType, variation, x, z }) => {
       return { landType, variation, location: { x, z } };
     });
-  }
-
-  async findOne({ x, z }: GetLocationParamDto) {
-    console.log({ x, z });
-    return {
-      type: 'empty' as const,
-    };
-  }
-
-  update(id: number) {
-    return `This action updates a #${id} location`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} location`;
   }
 }
